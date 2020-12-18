5d01906da9195c103ac9c905c625ee2daa3a24aa
r.buchinski <rozalin.buchinski@mentormate.com>
2020-12-18 14:52:44 +0200

    Add: component to test PR status check

--- lighthouse/src/app/app.module.ts
+++ lighthouse/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestingComponentComponent } from './testing-component/testing-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestingComponentComponent
   ],
   imports: [
     BrowserModule,
